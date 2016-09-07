754f40e07dee7878951ddd5b9a8e75f43243c414
John Sniezek <sniezekjp@aol.com>
2016-09-07 18:52:47 -0400

    Working on styles

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 import {Router, RouterConfiguration} from 'aurelia-router';
 
 export class App {
-  router;
+  //router;
+  //
+  //configureRouter(config, router){
+  //  config.title = 'App';
+  //  config.map([
+  //    { route: ['', 'browse'], name: 'browse', moduleId: './routes/browse', title: 'Browse', icon: 'language', nav: true },
+  //    { route: 'home', name: 'home', moduleId: './routes/home',   title: 'Home', icon: 'assignment_ind', nav: true }
+  //  ]);
+  //
+  //  this.router = router;
+  //}
 
-  configureRouter(config, router){
-    config.title = 'App';
-    config.map([
-      { route: ['', 'browse'], name: 'browse', moduleId: './routes/browse', title: 'Browse', icon: 'language', nav: true },
-      { route: 'home', name: 'home', moduleId: './routes/home',   title: 'Home', icon: 'assignment_ind', nav: true }
-    ]);
-
-    this.router = router;
+  toggleOptions() {
+    this.showMenu = !this.showMenu;
   }
 }
